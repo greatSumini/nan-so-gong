c2744c1511cbedfa60d36b38255a06979f6b112f
greatSumini <greatSumini@gmail.com>
2022-06-05 21:00:18 +0900

    feat(components): :sparkles: add common/Input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import { Layout, Button } from "@/components/common";
+import { Layout, Button, Input } from "@/components/common";
 
 const Title = styled.h1`
   margin: 0;
@@ -12,6 +12,12 @@ export default function HomePage() {
   return (
     <Layout>
       <Title>Hello 난소공!</Title>
+      <Input
+        label="새 비밀번호"
+        value="sdfdsdsf"
+        onChange={console.log}
+        type="password"
+      />
       <Button>안녕하세요!</Button>
     </Layout>
   );
---
+++ src/components/common/Input.tsx
@@ -0,0 +1,58 @@
+import { ChangeEventHandler, HTMLInputTypeAttribute } from "react";
+import styled from "styled-components";
+
+import { rem } from "@/helpers";
+
+type InputProps = {
+  label: string;
+  /** @default '100%'' */
+  width?: number | string;
+
+  type?: HTMLInputTypeAttribute;
+  value: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+};
+
+export function Input(props: InputProps) {
+  const { label, width, ...inputProps } = props;
+
+  return (
+    <Wrapper>
+      <LabelText>{label}</LabelText>
+      <StyledInput {...inputProps} />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div<{ width?: number | string }>`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  width: ${(props) => rem(props.width) ?? "100%"};
+  height: fit-content;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  height: 4.8rem;
+  padding-left: 1.6rem;
+
+  font-size: 2.4rem;
+
+  border: 0.1rem solid ${({ theme }) => theme.colors.gray5};
+  border-radius: 0.4rem;
+  outline: none;
+
+  transition: all 0.2s;
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.gray2};
+  }
+`;
+
+const LabelText = styled.label`
+  margin: 0 0 1.2rem 0;
+
+  color: ${({ theme }) => theme.colors.gray3};
+  font-size: 1.6rem;
+`;
