3f2fba1cf7cfa44382f77a5dc8f85cf14b9f6631
greatSumini <greatSumini@gmail.com>
2022-06-06 03:26:31 +0900

    feat(): add HomePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,54 @@
 import styled from "styled-components";
 
-import { Layout, Button, Input } from "@/components/common";
-
-const Title = styled.h1`
-  margin: 0;
-  font-size: 24px;
-  color: ${({ theme }) => theme.colors.red};
-`;
+import { Layout, Button } from "@/components/common";
 
 export default function HomePage() {
   return (
     <Layout>
-      <Title>Hello 난소공!</Title>
-      <Input
-        label="새 비밀번호"
-        value="sdfdsdsf"
-        onChange={console.log}
-        type="password"
-      />
-      <Button>안녕하세요!</Button>
+      <Logo src="/images/octo/default.png" />
+      <Title>난쏘마스크에 오신 것을 환영합니다!</Title>
+      <Description>
+        안전하고 투명한 시스템으로 빠르고
+        <br />
+        편리한 거래 환경을 제공합니다
+      </Description>
+      <Row>
+        <Button width={288}>지갑 생성</Button>
+        <Button width={288}>지갑 가져오기</Button>
+      </Row>
     </Layout>
   );
 }
+
+const Logo = styled.img`
+  margin-bottom: auto;
+
+  width: 20rem;
+  height: 15rem;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 2.4rem;
+
+  font-size: 3.2rem;
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.gray1};
+`;
+
+const Description = styled.p`
+  margin-bottom: 4.8rem;
+
+  text-align: center;
+
+  font-size: 2.4rem;
+  color: ${({ theme }) => theme.colors.gray4};
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  width: 100%;
+  margin-top: auto;
+`;
