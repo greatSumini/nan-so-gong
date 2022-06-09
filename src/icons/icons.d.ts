f5c02d1eb21cda872a8d7623ee12bba0a199739f
greatSumini <greatSumini@gmail.com>
2022-06-09 16:01:50 +0900

    feat(icons): :sparkles: add CopyIcon

---
+++ src/icons/icons.d.ts
@@ -0,0 +1,10 @@
+export type IconProps = {
+  className?: string;
+
+  style?: React.CSSProperties;
+  fill?: string;
+  fillIn?: string;
+  fillOut?: string;
+  fillDot?: string;
+  onClick?: React.MouseEventHandler<SVGSVGElement>;
+};
