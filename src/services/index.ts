3bcdc0086c61b890e08709a651f6f1a600d02587
greatSumini <greatSumini@gmail.com>
2022-06-09 20:20:39 +0900

    feat(services): :sparkles: add WalletService

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,2 @@
 export { default as AuthService } from "./auth.service";
+export { default as WalletService } from "./wallet.service";
---
+++ src/services/wallet.service.ts
@@ -0,0 +1,31 @@
+import { Wallet, Transaction } from "@/models";
+
+const wallets: { [privateKey: string]: Wallet } = {};
+
+class WalletService {
+  private getWallet(privateKey: string) {
+    const wallet = wallets[privateKey];
+    if (!wallet) {
+      throw new Error("일치하는 지갑을 찾을 수 없습니다.");
+    }
+    return wallet;
+  }
+  private addTransaction(
+    payload: Pick<Transaction, "hash" | "type" | "amount">,
+    privateKey: string
+  ) {
+    this.getWallet(privateKey).addTransaction(payload);
+  }
+
+  create(privateKey: string) {
+    wallets[privateKey] = new Wallet(privateKey);
+  }
+  receive(hash: string, amount: number, privateKey: string) {
+    this.addTransaction({ hash, type: "receive", amount }, privateKey);
+  }
+  send(hash: string, amount: number, privateKey: string) {
+    this.addTransaction({ hash, type: "send", amount }, privateKey);
+  }
+}
+
+export default new WalletService();
